0940773a710f2c5c340f5870e270827b43c267e8
Onur Erikçi <onrerkc@gmail.com>
2022-11-25 19:32:31 +0300

    First Release

---
+++ variables.py
@@ -0,0 +1,26 @@
+from os import path
+# ======================================================================================================================
+# root =================================================================================================================
+root = path.dirname(__file__)
+# ======================================================================================================================
+# İkonlar ==============================================================================================================
+browser_icon_x16 = path.join(root, 'assets\\x16\\browser.ico')
+homepage_icon_x16 = path.join(root, 'assets\\x16\\homepage.ico')
+back_icon_x16 = path.join(root, 'assets\\x16\\back.ico')
+forward_icon_x16 = path.join(root, 'assets\\x16\\forward.ico')
+refresh_icon_x16 = path.join(root, 'assets\\x16\\refresh.ico')
+mute_icon_x16 = path.join(root, 'assets\\x16\\mute.ico')
+unmute_icon_x16 = path.join(root, 'assets\\x16\\unmute.ico')
+download_icon_x16 = path.join(root, 'assets\\x16\\download.ico')
+show_music_icon_x16 = path.join(root, 'assets\\x16\\music.ico')
+show_video_icon_x16 = path.join(root, 'assets\\x16\\video.ico')
+search_icon_x16 = path.join(root, 'assets\\x16\\search.ico')
+find_icon_x16 = path.join(root, 'assets\\x16\\find.ico')
+youtube_icon_x16 = path.join(root, 'assets\\x16\\youtube.ico')
+info_icon_x16 = path.join(root, 'assets\\x16\\info.ico')
+zoom_out_icon_x16 = path.join(root, 'assets\\x16\\zoom_out.ico')
+zoom_in_icon_x16 = path.join(root, 'assets\\x16\\zoom_in.ico')
+zoom_icon_x16 = path.join(root, 'assets\\x16\\zoom.ico')
+close_icon_x16 = path.join(root, 'assets\\x16\\close.ico')
+
+key = "https://www.youtube.com/watch?v="
